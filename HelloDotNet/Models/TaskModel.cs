26f936d1dd9910c113d7537e0c55c656a3979fb3
Aung Ko Man <aungkoman@gmail.com>
2025-05-27 11:22:29 +0630

    model is added

---
+++ HelloDotNet/Models/TaskModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelloDotNet.Models
+{
+    public class TaskModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters.")]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [DataType(DataType.DateTime)]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
